76c73c8c49f35c8a7e1eac3a46381a42f69b2dc9
levy041096 <letranthaovy041096@gmail.com>
2016-12-28 13:58:48 -0800

    project dang ky

---
+++ DANGKY/DANGKY/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace DANGKY
+{
+    partial class frmDangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btDangky = new System.Windows.Forms.Button();
+            this.btHuy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnhaplaiMK = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.lbloi = new System.Windows.Forms.Label();
+            this.rdNTD = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdNTV = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbHienThi = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btDangky
+            // 
+            this.btDangky.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDangky.ForeColor = System.Drawing.Color.Blue;
+            this.btDangky.Location = new System.Drawing.Point(383, 411);
+            this.btDangky.Name = "btDangky";
+            this.btDangky.Size = new System.Drawing.Size(127, 38);
+            this.btDangky.TabIndex = 0;
+            this.btDangky.Text = "ĐĂNG KÝ";
+            this.btDangky.UseVisualStyleBackColor = true;
+            this.btDangky.Click += new System.EventHandler(this.btDangky_Click);
+            // 
+            // btHuy
+            // 
+            this.btHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btHuy.ForeColor = System.Drawing.Color.Blue;
+            this.btHuy.Location = new System.Drawing.Point(238, 411);
+            this.btHuy.Name = "btHuy";
+            this.btHuy.Size = new System.Drawing.Size(127, 38);
+            this.btHuy.TabIndex = 0;
+            this.btHuy.Text = "HỦY";
+            this.btHuy.UseVisualStyleBackColor = true;
+            this.btHuy.Click += new System.EventHandler(this.btHuy_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email *: ";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(98, 12);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(412, 31);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật Khẩu*: ";
+            // 
+            // txtMK
+            // 
+            this.txtMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMK.Location = new System.Drawing.Point(235, 62);
+            this.txtMK.Multiline = true;
+            this.txtMK.Name = "txtMK";
+            this.txtMK.PasswordChar = '*';
+            this.txtMK.Size = new System.Drawing.Size(245, 31);
+            this.txtMK.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nhập Lại Mật Khẩu*: ";
+            // 
+            // txtnhaplaiMK
+            // 
+            this.txtnhaplaiMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnhaplaiMK.Location = new System.Drawing.Point(235, 111);
+            this.txtnhaplaiMK.Multiline = true;
+            this.txtnhaplaiMK.Name = "txtnhaplaiMK";
+            this.txtnhaplaiMK.PasswordChar = '*';
+            this.txtnhaplaiMK.Size = new System.Drawing.Size(245, 31);
+            this.txtnhaplaiMK.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(12, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 22);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Số Điện Thoạil *: ";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSDT.Location = new System.Drawing.Point(238, 285);
+            this.txtSDT.Multiline = true;
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(245, 31);
+            this.txtSDT.TabIndex = 2;
+            // 
+            // lbloi
+            // 
+            this.lbloi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbloi.ForeColor = System.Drawing.Color.Red;
+            this.lbloi.Location = new System.Drawing.Point(235, 149);
+            this.lbloi.Name = "lbloi";
+            this.lbloi.Size = new System.Drawing.Size(245, 23);
+            this.lbloi.TabIndex = 3;
+            // 
+            // rdNTD
+            // 
+            this.rdNTD.AutoSize = true;
+            this.rdNTD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdNTD.ForeColor = System.Drawing.Color.Blue;
+            this.rdNTD.Location = new System.Drawing.Point(17, 28);
+            this.rdNTD.Name = "rdNTD";
+            this.rdNTD.Size = new System.Drawing.Size(141, 20);
+            this.rdNTD.TabIndex = 4;
+            this.rdNTD.TabStop = true;
+            this.rdNTD.Text = "Nhà Tuyển Dụng";
+            this.rdNTD.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdNTV);
+            this.groupBox1.Controls.Add(this.rdNTD);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(30, 330);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(453, 63);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hình Thức Đăng Ký";
+            // 
+            // rdNTV
+            // 
+            this.rdNTV.AutoSize = true;
+            this.rdNTV.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdNTV.ForeColor = System.Drawing.Color.Blue;
+            this.rdNTV.Location = new System.Drawing.Point(258, 28);
+            this.rdNTV.Name = "rdNTV";
+            this.rdNTV.Size = new System.Drawing.Size(132, 20);
+            this.rdNTV.TabIndex = 4;
+            this.rdNTV.TabStop = true;
+            this.rdNTV.Text = "Người Tìm Việc";
+            this.rdNTV.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(12, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 22);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Họ *: ";
+            // 
+            // txtHo
+            // 
+            this.txtHo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHo.Location = new System.Drawing.Point(235, 179);
+            this.txtHo.Multiline = true;
+            this.txtHo.Name = "txtHo";
+            this.txtHo.PasswordChar = '*';
+            this.txtHo.Size = new System.Drawing.Size(245, 31);
+            this.txtHo.TabIndex = 2;
+            this.txtHo.UseSystemPasswordChar = true;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(12, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(220, 22);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Tên (Hoặc Tên Công Ty) *: ";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTen.Location = new System.Drawing.Point(238, 232);
+            this.txtTen.Multiline = true;
+            this.txtTen.Name = "txtTen";
+            this.txtTen.PasswordChar = '*';
+            this.txtTen.Size = new System.Drawing.Size(245, 31);
+            this.txtTen.TabIndex = 2;
+            this.txtTen.UseSystemPasswordChar = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(12, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(214, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "đối với nhà tuyển dụng không cần nhập Họ";
+            // 
+            // cbHienThi
+            // 
+            this.cbHienThi.AutoSize = true;
+            this.cbHienThi.Location = new System.Drawing.Point(16, 154);
+            this.cbHienThi.Name = "cbHienThi";
+            this.cbHienThi.Size = new System.Drawing.Size(107, 17);
+            this.cbHienThi.TabIndex = 7;
+            this.cbHienThi.Text = "hiển thị mật khẩu";
+            this.cbHienThi.UseVisualStyleBackColor = true;
+            this.cbHienThi.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // frmDangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 461);
+            this.Controls.Add(this.cbHienThi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbloi);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtnhaplaiMK);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtHo);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btHuy);
+            this.Controls.Add(this.btDangky);
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.MaximizeBox = false;
+            this.Name = "frmDangKy";
+            this.Text = "ĐĂNG KÝ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btDangky;
+        private System.Windows.Forms.Button btHuy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnhaplaiMK;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Label lbloi;
+        private System.Windows.Forms.RadioButton rdNTD;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdNTV;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox cbHienThi;
+    }
+}
+
---
+++ DANGKY/DANGKY/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DANGKY
+{
+    public partial class frmDangKy : Form
+    {
+        string cnStr;
+        SqlConnection cn;
+        SqlCommand cmd = new SqlCommand();
+        public frmDangKy()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cnStr = @"server = THAOVY-PC\LEVY; Database = HT_tuyendung; Integrated security = true;";
+        }
+
+        private void Connect()
+        {
+            if (cn != null && cn.State == ConnectionState.Closed)
+            {
+                cn.Open();
+            }
+        }
+        private void Disconnect()
+        {
+            if (cn == null && cn.State != ConnectionState.Closed)
+            {
+                cn.Close();
+            }
+        }
+
+        private void btHuy_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btDangky_Click(object sender, EventArgs e)
+        {
+            Connect();
+            try
+            {
+                cn = new SqlConnection(cnStr);
+                cn.Open();
+                cmd = new SqlCommand();
+                if (rdNTV.Checked == true)
+                {
+                    if (txtnhaplaiMK.Text == txtMK.Text && txtMK.Text != null) //&& txtEmail.Text != null&& txtMK.Text != null && txtTen.Text == null && txtHo.Text == null)
+                    {
+                        cmd.CommandText = "usp_taikhoanNTV";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = cn;
+                        cmd.Parameters.Add(new SqlParameter("@Ho", txtHo.Text));
+                        cmd.Parameters.Add(new SqlParameter("@Ten", txtTen.Text));
+                        cmd.Parameters.Add(new SqlParameter("@Email", txtEmail.Text));
+                        cmd.Parameters.Add(new SqlParameter("@MKhau", txtMK.Text));
+                        cmd.Parameters.Add(new SqlParameter("@DienThoai", txtSDT.Text));
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("ĐĂNG NHẬP THÀNH CÔNG", "thong bao");
+                        Application.Exit();
+                        
+                        
+                        
+                    }
+                    else if (txtnhaplaiMK.Text != txtMK.Text || txtnhaplaiMK.Text == null || txtMK.Text == null)
+                    {
+                        lbloi.Show();
+                        lbloi.Text = "Nhập lại đúng mật khẩu";
+                    }
+                }
+                else if (rdNTD.Checked == true)
+                {
+
+                    if (txtnhaplaiMK.Text == txtMK.Text) //&& txtEmail.Text != null && txtMK.Text != null && txtTen.Text == null)
+                    {
+                        cmd.CommandText = "usp_taikhoanNTD";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = cn;
+                        cmd.Parameters.Add(new SqlParameter("@Ten", txtTen.Text));
+                        cmd.Parameters.Add(new SqlParameter("@Email", txtEmail.Text));
+                        cmd.Parameters.Add(new SqlParameter("@MKhau", txtMK.Text));
+                        cmd.Parameters.Add(new SqlParameter("@DienThoai", txtSDT.Text));
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("ĐĂNG KÝ THÀNH CÔNG");
+                        Application.Exit();
+                    }
+                    else if (txtnhaplaiMK.Text != txtMK.Text || txtnhaplaiMK.Text == null)
+                    {
+                        lbloi.Show();
+                        lbloi.Text = "Nhập lại đúng mật khẩu";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("BẠN LÀ NHÀ TUYỂN DỤNG HAY NGƯỜI TÌM VIỆC?");
+                }
+                
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                //throw;
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbHienThi.Checked)
+            {
+                txtMK.UseSystemPasswordChar = true;
+                txtnhaplaiMK.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                txtMK.UseSystemPasswordChar = false;
+                txtnhaplaiMK.UseSystemPasswordChar = false;
+            }
+        }
+        //private int KT_EmailNTV()
+        //{
+        //    Connect();
+        //    try
+        //    {
+        //        cn = new SqlConnection(cnStr);
+        //        cn.Open();
+        //        cmd = new SqlCommand();
+        //        cmd = new SqlCommand();
+        //        cmd.CommandText = "USP_emailNTV";
+        //        cmd.CommandType = CommandType.StoredProcedure;
+        //        cmd.Connection = cn;
+        //        cmd.Parameters.Add(new SqlParameter("@Email", txtEmail.Text));
+        //        int x = (int)cmd.ExecuteScalar();
+        //        return x;
+        //        //if (x >= 2)
+        //        //{
+        //        //    MessageBox.Show("EMAIL ĐÃ CÓ NGƯỜI DÙNG");   
+        //        //}
+        //        //else
+        //        //{
+        //        //    MessageBox.Show("ĐĂNG NHẬP THÀNH CÔNG", "thong bao");
+        //        //    Application.Exit();
+        //        //}
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //        //throw;
+        //    }
+        //    finally
+        //    {
+        //        Disconnect();
+        //    }
+           
+        //}
+
+        
+    }
+}
